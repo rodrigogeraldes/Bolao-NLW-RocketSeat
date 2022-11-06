36525741c9b8e2e1867d892ecca33f2bff33f1d9
rodrigo.geraldes <rodrigo.geraldes@aluno.ifsp.edu.br>
2022-11-06 03:19:28 -0300

    web finalizado

---
+++ server/prisma/seed.ts
@@ -0,0 +1,62 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main() {
+    const user = await prisma.user.create({
+        data:{
+            name: 'John Doe',
+            email: 'john.doe@gmail.com',
+            avatarUrl: 'https://github.com/rodrigogeraldes.png'
+        }
+    })
+
+    const pool = await prisma.pool.create({
+        data: {
+            title: 'Example Pool',
+            code: 'BOL123',
+            ownerId: user.id,
+
+        
+            Participant:{
+                create:{
+                    userId: user.id
+                }
+            }
+        }
+    })
+
+    await prisma.game.create({
+        data:{
+            date: '2022-11-04T14:03:53.201Z',
+            firstTeamCountryCode: 'DE',
+            secondTeamCountryCode: 'BR',
+        }
+    })
+
+    await prisma.game.create({
+        data:{
+            date: '2022-11-03T14:03:53.201Z',
+            firstTeamCountryCode: 'BR',
+            secondTeamCountryCode: 'AR',
+
+            guesses:{
+                create:{
+                    firsTeamPoints: 2,
+                    secondTeamPoints: 1,
+                    participant:{
+                        connect:{
+                            userId_poolId: {
+                                userId: user.id,
+                                poolId: pool.id,
+                            }
+                        }
+                    }
+                }
+        }
+    }
+})
+    
+}
+
+main()
